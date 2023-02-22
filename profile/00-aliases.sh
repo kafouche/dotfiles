7876159bd75e2756d571096a2ba287fa9d3051e3
pacemoici <106478708+pacemoici@users.noreply.github.com>
2023-02-22 18:07:32 +0100

    Update 00-aliases.sh

--- profile/00-aliases.sh
+++ profile/00-aliases.sh
@@ -1,2 +1,2 @@
 alias ll='ls --all --human-readable --list'
-alias llblk='lsblk --all --options NAME,TYPE,RM,FSTYPE,RO,SIZE,FSUSED,FSUSE%,MOUNTPOINT'
+alias llblk='lsblk --output NAME,TYPE,RM,FSTYPE,RO,SIZE,FSUSED,FSUSE%,MOUNTPOINT'
